3a5c17eafe3147f0e7a7c5c95f370f4122ecab2f
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 11:49:35 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan2-DeklarasiVaribel/src/pbo1/pkg10118031/latihan2/deklarasipengaksesanvariabel/PBO110118031Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,31 @@
+package pbo1.pkg10118031.latihan2.deklarasipengaksesanvariabel;
+
+/**
+ *
+ * @author Vendetta 
+ * NAMA : RIZKI ARIA PUTRA 
+ * KELAS : PBO1 
+ * NIM : 10118031
+ * Deskripsi Program : Program ini berisi program untuk deklarasi variabel
+ */
+public class PBO110118031Latihan2DeklarasiPengaksesanVariabel {
+
+    public static void main(String[] args) {
+        // Deklarasi Varibel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        //Menampilkan Hasil
+        System.out.println("Isi varibel nilai = " + nilaiInt);
+        System.out.println("Isi varibel PHI = " + PHI);
+        System.out.println("Isi varibel Logika = " + nilaiLogika);
+        System.out.println("Isi varibel Karakter = " + nilaiKarakter);
+    }
+}
